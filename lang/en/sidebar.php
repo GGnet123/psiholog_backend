a5c4567404110661c4aa5a86f8279b1653a2683b
hmurich <hmurich@mail.ru>
2022-03-21 23:03:09 +0600

    done sidebars

--- lang/en/sidebar.php
+++ lang/en/sidebar.php
@@ -4,6 +4,7 @@ return [
     'content' => 'Контент',
     'admin_faq' => 'FAQ',
     'libs' => 'Справочники',
+    'admin_index' => 'Главная',
     'admin_lib_spec' => 'Специализаций',
     'admin_term' => 'Условия использования',
     'sample_video' => 'Пример видео звонка',
