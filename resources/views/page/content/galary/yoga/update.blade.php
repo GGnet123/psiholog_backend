3d226bc78f204c13f423f6b5c1b75bb0b0ff7ee7
hmurich <hmurich@mail.ru>
2022-06-17 16:53:48 +0600

    d

--- resources/views/page/content/galary/yoga/update.blade.php
+++ resources/views/page/content/galary/yoga/update.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <x-form.update :title="$title" :action="route($route_path.'_update_save', $model)">
             <x-input.select name="cat_id" :model="$model"
-                            :ar="[null=>$model->label('null_cat')] + App\Models\Content\MainGalaryCat::where('type', 'TYPE_YOGA_TO_ME')->pluck('id', 'title')->toArray()" />
+                            :ar="[null=>$model->label('null_cat')] + App\Models\Content\MainGalaryCat::where('type', 'TYPE_YOGA_TO_ME')->pluck('title', 'id')->toArray()" />
             <x-input.text name="title" :model="$model" required  />
             <x-input.text name="title_en" :model="$model" required  />
             <x-input.text name="google_drive_music" :model="$model" required  />
