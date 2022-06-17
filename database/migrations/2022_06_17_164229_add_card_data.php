b3e5b90089b83a89c9ac2ae7e2b1a86bd6dee4c7
hmurich <hmurich@mail.ru>
2022-06-17 16:54:57 +0600

    d

--- database/migrations/2022_06_17_164229_add_card_data.php
+++ database/migrations/2022_06_17_164229_add_card_data.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         //
         Schema::table('users', function (Blueprint $table) {
-            $table->text('card_data')->default(false);
+            $table->text('card_data')->nullable();
         });
     }
 
