0970bef64f97b0f77a322ea2c29f6531c13b86ca
hmurich <hmurich@gmail.com>
2022-12-10 12:09:59 +0600

    d

--- resources/views/page/content/galary/night_story/update.blade.php
+++ resources/views/page/content/galary/night_story/update.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <x-form.update :title="$title" :action="route($route_path.'_update_save', $model)">
             <x-input.select name="cat_id" :model="$model"
-                            :ar="[null=>$model->label('null_cat')] + App\Models\Content\MainGalaryCat::where('type', 'TYPE_YOGA_TO_ME')->pluck('title', 'id')->toArray()" />
+                            :ar="[null=>$model->label('null_cat')] + App\Models\Content\MainGalaryCat::where('type', 'TYPE_NIGHT_STORY')->pluck('title', 'id')->toArray()" />
             <x-input.text name="title" :model="$model" required  />
             <x-input.text name="title_en" :model="$model" required  />
             <x-input.file name="image_id"  :model="$model"   />
