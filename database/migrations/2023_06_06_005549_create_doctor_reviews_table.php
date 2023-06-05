c5abeb4ff102c91ed841e3823abf7eb768bddd91
Галым Жарысов <g.zharysov@affstar.com>
2023-06-06 00:57:48 +0600

    doctor reviews table

--- database/migrations/2023_06_06_005549_create_doctor_reviews_table.php
+++ database/migrations/2023_06_06_005549_create_doctor_reviews_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('doctor_reviews', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('rate');
+            $table->string('comment')->nullable();
             $table->timestamps();
         });
     }
